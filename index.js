f3b73ede9ff1e8cfbde66a0d84a9e5c83618d5b4
Ravi Vishwakarma <ravi.vishwakarma.work@gmail.com>
2025-03-13 22:27:35 +0530

    Added React and ReactDOM CDN to the project and injected heading with React JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { className: "heading" },
+  "Jay Shree Krishna"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
