d3d6854055a147190ded7985392b48affd3ce408
Ravi Vishwakarma <ravi.vishwakarma.work@gmail.com>
2025-03-14 20:26:31 +0530

    Created nested tag structure with React.createElement also applied CSS

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const heading = React.createElement(
   { className: "heading" },
   "Jay Shree Krishna"
 );
+const paragraph = React.createElement(
+  "p",
+  { className: "paragraph" },
+  "Lorem ipsum is a simply dummy text."
+);
+
+const div = React.createElement("div", { className: "div" }, "Parent Div", [
+  heading,
+  paragraph,
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(div);
